8bdc173c8a546b12f6d66225ca521b7bfb287c0c
Santosh Kumar <sntshkmr60@gmail.com>
2021-12-14 05:36:10 +0530

    Tweak to test github<->Jenkins connetion

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -6,5 +6,8 @@ class TestHW(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(cotu.hello_world(), "hello world")
 
+    def test_hello_world_on_jenkins(self):
+        self.assertEqual(cotu.hello_world(), "hello jenkins")
+
 if __name__ == '__main__':
     unittest.main()
